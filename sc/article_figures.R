0a73c9c890e03d53eb8d643b28034976d7f2d45f
Carlos Ramirez Alvarez <cramirezal@ciencias.unam.mx>
2019-10-08 01:23:39 -0500

    adding t test

--- sc/article_figures.R
+++ sc/article_figures.R
@@ -1,14 +1,39 @@
-## data analysis on the roll of inflammation in obesity 
+## obese analysis on the roll of inflammation in obesity 
 ## based on a cohort of eutrofic and obese patients
+setwd(getwd())
 
 ## Dependencies
 library(googlesheets)
 library(dplyr)
 library(ggplot2)
 
-## loading data
-data <- gs_title('ObesidadHGMcompleto20191004') %>%
+## loading obese chorot data from google spreadsheet
+obese <- gs_title('ObesidadHGMcompleto20191004') %>%
               gs_read('BASE DE DATOS A')
 
-## Data preprocessing
-data$Obeso
+## obese data preprocessing
+## removing empty spaces in col names
+names(obese) <- gsub(' ', '_', names(obese)) 
+## dropping non sense variables as ids
+obese.p <- select(obese, 
+                 -FCS,
+                 -CLAVE,
+                 -contains('FECHA')
+                 )    %>%
+          mutate(
+                  ## defining obese category from Tipo de paciente col
+                 obesity = grepl('obeso', tolower(Tipo_de_paciente)),
+                  ## getting adult category from tipo de paciente col
+                 adult = grepl('adult', tolower(Tipo_de_paciente))
+          )
+
+vals <- with(obese.p,
+    tapply(NEUTROFILOS, obesity, as.vector)        
+)
+tt <- t.test(vals$'FALSE', vals$'TRUE')
+tt$p.value
+
+with(obese.p,
+     boxplot(NEUTROFILOS ~ obesity,
+             col = c('green', 'red'))
+)
