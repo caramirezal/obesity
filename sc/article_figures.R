55b6cad06460c705734cbf194738554256f5f60f
Carlos Ramirez <cramireza@ciencias.unam.mx>
2020-01-06 18:16:07 +0100

    updating imputation

--- sc/article_figures.R
+++ sc/article_figures.R
@@ -1,39 +1,74 @@
 ## obese analysis on the roll of inflammation in obesity 
 ## based on a cohort of eutrofic and obese patients
-setwd(getwd())
 
 ## Dependencies
-library(googlesheets)
-library(dplyr)
-library(ggplot2)
+library(googlesheets)   ## connection to google spreadsheets
+library(dplyr)          ## Data processing
+library(ggplot2)        ## Plotting
+library(VIM)            ## Variable imputation
+library(missForest)     ## imputation using random forest
+library(Hmisc)
 
 ## loading obese chorot data from google spreadsheet
 obese <- gs_title('ObesidadHGMcompleto20191004') %>%
               gs_read('BASE DE DATOS A')
+
+## Removing annoying non alpha numeric characters in names
+colnames(obese) <- gsub('[^[:alnum:]]', '', colnames(obese))
 
 ## obese data preprocessing
 ## removing empty spaces in col names
 names(obese) <- gsub(' ', '_', names(obese)) 
 ## dropping non sense variables as ids
 obese.p <- select(obese, 
-                 -FCS,
-                 -CLAVE,
-                 -contains('FECHA')
-                 )    %>%
-          mutate(
-                  ## defining obese category from Tipo de paciente col
-                 obesity = grepl('obeso', tolower(Tipo_de_paciente)),
-                  ## getting adult category from tipo de paciente col
-                 adult = grepl('adult', tolower(Tipo_de_paciente))
-          )
+                  LymphocytesCount:LogCD8TEMQ3HLADRCD38MedianPEDZL
+                 )   %>%
+        select_if(is.numeric)
 
-vals <- with(obese.p,
-    tapply(NEUTROFILOS, obesity, as.vector)        
-)
-tt <- t.test(vals$'FALSE', vals$'TRUE')
-tt$p.value
+###########################################################################
+## Assessing the number of NAs in data
 
-with(obese.p,
-     boxplot(NEUTROFILOS ~ obesity,
-             col = c('green', 'red'))
-)
+## visualising the content of NAs
+aggr(obese.p, 
+     col=c('navyblue','yellow'),
+     numbers=TRUE, 
+     sortVars=TRUE,
+     labels=names(obese.p), 
+     cex.axis=.7,
+     gap=3, 
+     ylab=c("Missing data","Pattern"))
+
+
+## calculating the fraction of NAs in cols
+na_fracts <- apply(obese.p, 2, function(x) sum(is.na(x)) / length(x))
+
+## removing cols with fraction of NAs > 0.13
+threshold <- 0.13
+obese.filtered <- obese.p[, na_fracts < threshold]
+
+#############################################################################
+## Assessing variable imputation
+
+obese.m <- as.matrix(obese.filtered)
+
+## Imputing data using random forest
+obese.imputed <- missForest(obese.m)
+obese.imputed$OOBerror
+
+## Imputation with median values
+obese_Hmisc <- as.data.frame(sapply(obese.filtered, 
+                                        impute, mean))
+
+write.table(as.data.frame(obese.imputed$ximp), 
+            '../data/obesity_forest_imputed.tsv',
+            row.names = FALSE,
+            sep = '\t')
+
+write.table(obese_Hmisc, 
+            '../data/obesity_mean_imputed.tsv',
+            row.names = FALSE,
+            sep = '\t')
+
+
+
+
